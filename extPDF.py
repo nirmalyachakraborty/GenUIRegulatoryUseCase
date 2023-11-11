24336240d4087ba8e2a1d57d4bcce7e97893038e
TheBuffer <anishkonar10@gmail.com>
2023-11-11 21:17:50 +0530

    updating page wise jsons and extraction code

--- extPDF.py
+++ extPDF.py
@@ -19,44 +19,28 @@ class ScheduleInfo(BaseModel):
     key: str
     value: Optional[str]
 
-# class Overview(BaseModel):
-#     """Overview of a section of text."""
-#     summary: str = Field(description="Provide a concise summary of the content.")
-#     keywords: str = Field(description="Provide keywords related to the content.")
 
 class Schedule(BaseModel):
     """Information about Schedule in the report"""
+    pageNumber: str = Field(description="Provide the page number.")
+    scheduleName: str = Field(description="Provide the Schedule name.")
     schedules: List[ScheduleInfo]
 
 
-# overview_tagging_function = [
-#     convert_pydantic_to_openai_function(Overview)
-# ]
-
-# summary_model = model.bind(
-#     functions=overview_tagging_function,
-#     function_call={"name":"Overview"}
-# )
-# prompt = ChatPromptTemplate.from_messages([
-#     ("system", "Extract the relevant information as summary in one paragraph. Extract the keywords "),
-#     ("human", "{input}")
-# ])
-# summary_chain = prompt | summary_model | JsonOutputFunctionsParser()
-#
-# print(summary_chain.invoke({"input": page_content_inp}))
 
 schedule_tagging_function = [
     convert_pydantic_to_openai_function(Schedule)
 ]
 
-template = """A financial report will be passed to you. Extract from it the schedule info as key value pairs. 
+template = """A financial report will be passed to you. Extract from it the schedule info as key value pairs with the schedule description as schedule name. 
 Do not consider the Item number as key, read the key description as key, and the value in the table as value.
-Use the numeric or alphanumeric value as value only, do not add the item number as value.
+Use the numeric or alphanumeric value as value only, do not add the item number as value. If the value is not present, then use "N/A" as value.
+Create proper Json with double quotes.
 Do not make up or guess ANY extra information."""
 
 
 
-def getStructInfo(file_path,no_pages):
+def getStructInfo(file_path,no_pages,json_file_name):
     """Get the structure of the financial report.
     Parameters
     ----------
@@ -64,10 +48,8 @@ def getStructInfo(file_path,no_pages):
         Path to the financial report.
     no_pages : int
         Number of pages to extract.
-    Returns
-    -------
-    List[Schedule]
-        List of schedules extracted from the financial report.
+    json_file_name: str
+        Path to the output json.
     """
     loader = PyPDFLoader(file_path)
     documents = loader.load()
@@ -79,7 +61,7 @@ def getStructInfo(file_path,no_pages):
         ("human", "{input}")
     ])
 
-    model = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=1.2, max_tokens=2000)
+    model = ChatOpenAI(model_name="gpt-4", temperature=1.8, max_tokens=4000, model_kwargs={'top_p':0.5})
     # model_kwargs={'top_p':0.5}
 
     extraction_model = model.bind(
@@ -90,35 +72,27 @@ def getStructInfo(file_path,no_pages):
     jsonCurr=[]
     text = ""
     ctr = 0
+    #iterate through the document array and for each page, extract the JSON info and write to a file
     for doc in documents[:no_pages]:
        print("EXTRACTING INFO FROM ###############  PAGE ######################################### " + str(ctr))
-       text += doc.page_content
+
        ctr+=1
        extraction_chain = prompt | extraction_model | JsonOutputFunctionsParser()
        jsonDoc = extraction_chain.invoke({"input": doc.page_content})
-       json.dumps(jsonCurr.append(jsonDoc))
-    return jsonCurr, text
+       #print(jsonDoc)
+       file_path = json_file_name+str(ctr)+".json"
+       with open(file_path, "w") as outfile:
+           outfile.write(json.dumps(jsonDoc))
+
+    return
 
 
-CURR_DOC, CURR_DOC_TXT = getStructInfo("FR_Y-9C20230930_file Current.pdf",7)
-
-CURR_DOC_STR=" ".join(map(str,CURR_DOC))
-
-with open("CURR_JSON.txt", "w") as outfile:
-    outfile.write("".join(CURR_DOC_STR))
-
-# with open("CURR_TEXT.txt", "w") as outfile:
-#     outfile.write(CURR_DOC_TXT)
+getStructInfo("FR_Y-9C20230630_file_June.pdf",7,"OLD_JSON")
 
 print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 
-OLD_DOC, OLD_DOC_TXT = getStructInfo("FR_Y-9C20230630_file_June.pdf",7)
-OLD_DOC_STR=" ".join(map(str,OLD_DOC))
+getStructInfo("FR_Y-9C20230930_file Current.pdf",7,"CURR_JSON")
 
-with open("OLD_JSON.txt", "w") as outfile:
-    outfile.write("".join(OLD_DOC_STR))
 
-# with open("OLD_old.txt", "w") as outfile:
-#     outfile.write(OLD_DOC_TXT)
 
 
